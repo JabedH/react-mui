fe046bc6a021c73b519a2baf5a23e0a162d11672
JabedH <jabedhtusar@gmail.com>
2022-10-25 16:53:29 +0600

    added home section

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,49 @@
-import { AppBar, styled, Toolbar, Typography } from "@mui/material";
-import React from "react";
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  InputBase,
+  Menu,
+  MenuItem,
+  styled,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
 import PetsIcon from "@mui/icons-material/Pets";
+import { Mail, Notifications } from "@mui/icons-material";
+import { display } from "@mui/system";
 
 const StyledToolbar = styled(Toolbar)({
   display: "flex",
   justifyContent: "space-between",
   margin: 10,
 });
-
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%",
+}));
+const Icon = styled(Box)(({ theme }) => ({
+  display: "none",
+  alignItems: "center",
+  gap: "20px",
+  [theme.breakpoints.up("sm")]: {
+    display: "flex",
+  },
+}));
+const UserBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+  [theme.breakpoints.up("sm")]: {
+    display: "none",
+  },
+}));
 const NavBar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <AppBar position="sticky">
       <StyledToolbar>
@@ -16,6 +51,48 @@ const NavBar = () => {
           nav bar
         </Typography>
         <PetsIcon sx={{ display: { xs: "block", sm: "none" } }} />
+        <Search>
+          <InputBase placeholder="search..." />
+        </Search>
+        <Icon>
+          <Badge badgeContent={4} color="error">
+            <Mail color="white" />
+          </Badge>
+          <Badge badgeContent={4} color="error">
+            <Notifications color="white" />
+          </Badge>
+          <Avatar
+            onClick={(e) => setOpen(true)}
+            sx={{ width: 30, height: 30 }}
+            src="/static/images/avatar/2.jpg"
+          />
+        </Icon>
+        <UserBox>
+          <Avatar
+            onClick={(e) => setOpen(true)}
+            sx={{ width: 30, height: 30 }}
+            src="/static/images/avatar/2.jpg"
+          />
+          <Typography>John</Typography>
+        </UserBox>
+        <Menu
+          id="demo-positioned-menu"
+          aria-labelledby="demo-positioned-button"
+          open={open}
+          onClose={(e) => setOpen(false)}
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+        >
+          <MenuItem>Profile</MenuItem>
+          <MenuItem>My account</MenuItem>
+          <MenuItem>Logout</MenuItem>
+        </Menu>
       </StyledToolbar>
     </AppBar>
   );
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,16 +1,32 @@
-import { Box } from "@mui/material";
+import { Home } from "@mui/icons-material";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
 import React from "react";
 
 const Sidebar = () => {
   return (
     <Box
-      bgcolor="lightblue"
       flex={1}
       p={2}
       textAlign="center"
       sx={{ display: { xs: "none", sm: "block" } }}
     >
-      Sidebar
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <Home />
+            </ListItemIcon>
+            <ListItemText primary="HomePage" />
+          </ListItemButton>
+        </ListItem>
+      </List>
     </Box>
   );
 };
